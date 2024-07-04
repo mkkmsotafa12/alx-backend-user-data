92e5dab61b7afc884c91954e23651c1ecff2e884
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-04 16:32:20 -0400

    6. Check valid password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module for hashing and validating passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Expects one string argument name password and returns a salted,
+        hashed password, which is a byte string """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Expects 2 arguments and returns a boolean """
+    return bcrypt.checkpw(password.encode(), hashed_password)
