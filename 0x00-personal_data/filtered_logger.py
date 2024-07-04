73cc47578c624ef912cfeaea8b6bc911b2deb7f6
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-04 16:27:41 -0400

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
-    """ Return the log message obfuscated """
+    """ Returns the log message obfuscated """
     for item in fields:
         message = re.sub(fr'{item}=.+?{separator}',
                          f'{item}={redaction}{separator}', message)
