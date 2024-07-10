94568919e05f003cde58acc2b5c15f120b533948
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-10 07:10:08 -0400

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,7 +37,7 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(self, decoded_base64_d: str) -> (str, str):
-        """ exctract user credentials """
+        """ Exctract user credentials """
         if not decoded_base64_d:
             return (None, None)
         elif type(decoded_base64_d) is not str:
