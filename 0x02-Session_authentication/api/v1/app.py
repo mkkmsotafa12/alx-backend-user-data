e37925453cc623bbdbe2499bcc2d46453b9fef86
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-13 08:18:33 -0400

    1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,18 +42,23 @@ def forbid(error):
 
 
 @app.before_request
-def check_auth():
-    """ Function to edit auth variable """
-    if not auth:
-        return
-    list_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, list_paths):
-        return
-    elif auth.authorization_header(request) is None:
-        abort(401)
-    elif auth.current_user(request) is None:
-        abort(403)
+def authenticate_user():
+    """ Authenticates a user before processing a request """
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
