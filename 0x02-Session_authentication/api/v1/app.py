faaa9790d006071e36e76ad0f8266abdeef936ce
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-13 08:27:03 -0400

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,6 +13,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
+
 if getenv('AUTH_TYPE'):
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -21,51 +22,43 @@ if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-if getenv('AUTH_TYPE') == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-
-if getenv('AUTH_TYPE') == 'session_exp_auth':
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def aunauth(error):
-    """ unauthorized access """
+    """ Unauthorized access """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbid(error):
-    """ not allowed access """
+    """ Not allowed access """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def check_auth():
-    """ function to edit auth variable """
-    if not auth:
-        return
-    list_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if not auth.require_auth(request.path, list_paths):
-        return
-    elif auth.authorization_header(request) is None and \
-            auth.session_cookie(request) is None:
-        abort(401)
-    elif auth.authorization_header(request) and auth.session_cookie(request):
-        return None, 401
-    elif auth.current_user(request) is None:
-        abort(403)
-    request.current_user = auth.current_user(request)
+def authenticate_user():
+    """ Authenticates a user before processing a request """
+    if auth:
+        excluded_paths = [
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
