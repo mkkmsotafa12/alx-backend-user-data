f62052fc5c0d399e617365ac3e4d35af3b14e83b
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-13 08:28:11 -0400

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,8 +12,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
-        """
+        """ Creates a session id for the user """
         if type(user_id) is str:
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
