c0243c01a6672be52a8981bce93c748ee058115d
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-07-13 09:05:13 -0400

    9. Expiration?

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,18 +9,13 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """
-    A class used to manage session-based user authentication.
-
-    Attributes
-    ----------
-    user_id_by_session_id : dict
-        A dictionary that maps session IDs (str) to user IDs (str).
+    A class used to manage session-based user authentication
     """
 
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a new session for a user."""
+        """ Creates a new session for a user """
         if user_id is None or not isinstance(user_id, str):
             return None
 
@@ -29,20 +24,20 @@ class SessionAuth(Auth):
         return str(gen_id)
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Get the user id from the session."""
+        """ Get the user id from the session """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """Get a User based on his session ID."""
+        """ Get a User based on his session ID """
         cookie_value = self.session_cookie(request)
         user_id = self.user_id_for_session_id(cookie_value)
         curr_user = User.get(user_id)
         return curr_user
 
     def destroy_session(self, request=None):
-        """Deletes a user session."""
+        """ Deletes a user session """
         if request:
             cookie_value = self.session_cookie(request)
             user_id = self.user_id_for_session_id(cookie_value)
